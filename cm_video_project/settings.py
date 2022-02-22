79768cc471312a79edab1ad3177f75996faef029
ryoheikamata <r.k.pitschi@gmail.com>
2022-02-22 20:06:27 +0900

    change settings.py allowedhost

--- cm_video_project/settings.py
+++ cm_video_project/settings.py
@@ -32,7 +32,7 @@ DEBUG = env('DEBUG')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS')
 
 
 # Application definition

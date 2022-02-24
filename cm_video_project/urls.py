097904d4fc790c2df11893ac5520f4def09565d0
ryoheikamata <r.k.pitschi@gmail.com>
2022-02-24 19:28:34 +0900

    settiing urls static

--- cm_video_project/urls.py
+++ cm_video_project/urls.py
@@ -28,3 +28,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += [re_path(r'^static/(?P<path>.*)$', views.serve),]

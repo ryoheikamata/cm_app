9e81c88eb08ad64b3e6af3b90d21f1623c11772b
ryoheikamata <r.k.pitschi@gmail.com>
2022-02-28 16:46:07 +0900

    static

--- cm_video_project/settings.py
+++ cm_video_project/settings.py
@@ -140,8 +140,7 @@ STATICFIELS_DIRS = [
 # collectstaticコマンド。
 # 集める場所をsettings.pyに設定する必要がある。
 # (プロジェクト直下、"static"というフォルダを指定。)
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = str(BASE_DIR / 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # デプロイ環境で、{% static 'main.video' %}などのDjangoテンプレートが
 # 読みに行く場所をSTATIC_URLで指定
